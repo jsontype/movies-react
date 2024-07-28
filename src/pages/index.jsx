bd5f61ae7722f395a4d6b88fe4924a72cdf59059
TamakiSaito <immacd1012@gmail.com>
2024-07-28 18:59:39 +0900

    1234

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,6 +13,7 @@ const OTT = memo(() => {
   const [moviesSortByRating, setMoviesSortByRating] = useState([])
   // const [movies, setMovies] = useState([])
 
+  // ***! 
   useEffect(() => {
     fetch('https://yts.mx/api/v2/list_movies.json?sort_by=year')
       .then(res => {
@@ -22,16 +23,77 @@ const OTT = memo(() => {
         setMoviesSortByYear(json.data.movies)
       })
   }, [])
+  // useEffect(() => {
+  //   fetch('https://yts.mx/api/v2/list_movies.json?sort_by=year')
+  //     .then(res => res.json())
+  //     .then(json => {
+  //       const movies = json.data.movies;
+        
+  //       // Check Every Image URL 
+  //       const imagePromises = movies.map(movie => 
+  //         fetch(movie.large_cover_image)
+  //           .then(res => {
+  //             if (!res.ok) {
+  //               throw new Error('Image not found');
+  //             }
+  //             return movie;
+  //           })
+  //           .catch(() => ({...movie, large_cover_image: ''}))
+  //       );
+  
+  //       // Upadte After All Promise
+  //       Promise.all(imagePromises)
+  //         .then(updatedMovies => {
+  //           setMoviesSortByYear(updatedMovies);
+  //         });
+  //     })
+  //     .catch(error => {
+  //       console.error('Error fetching movies:', error);
+  //     });
+  // }, []);  
 
+  // useEffect(() => {
+  //   fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+  //     .then(res => {
+  //       return res.json()
+  //     })
+  //     .then(json => {
+  //       setMoviesSortByRating(json.data.movies)
+  //     })
+  // }, [])
   useEffect(() => {
-    fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
-      .then(res => {
-        return res.json()
-      })
+    fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating', { mode: 'no-cors' })
+      .then(res => res.json())
       .then(json => {
-        setMoviesSortByRating(json.data.movies)
+        const movies = json.data.movies;
+  
+        // Check Every Image URL
+        const imagePromises = movies.map(movie =>
+          fetch(movie.large_cover_image, { mode: 'no-cors' })
+            .then(res => {
+              console.log('res: ', res)
+              console.log('res.status: ', res.status)
+              if (!res.ok) {
+                throw new Error(`Image not found, status code: ${res.status}`);
+              }
+              return movie;
+            })
+            .catch(error => {
+              console.error('Fetch error:', error);
+              return { ...movie, large_cover_image: '' };
+            })
+        );
+  
+        // Update After All Promises
+        Promise.all(imagePromises)
+          .then(updatedMovies => {
+            setMoviesSortByRating(updatedMovies);
+          });
       })
-  }, [])
+      .catch(error => {
+        console.error('Error fetching movies:', error);
+      });
+  }, []);
 
   return (
     <>
